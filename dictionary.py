4bc66629da9b36e1ca9b21556fe5acf713200458
nikshitak <nikkikurva@gmail.com>
2023-02-04 20:11:31 -0600

    love

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,5 @@
 import requests
 import json
-from django.core.serializers import serialize
-
-
 
 word = "word"
 params = {
@@ -10,5 +7,6 @@ params = {
 }
 response = requests.get("https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + word + "?key=5887e7e6-989a-441a-8f13-2be340a0cd99", params=params)
 
-data = json.dumps(response, default=str)
-print(data)
+json_response = response.json()['shortdef']
+y = json.loads(json_response)
+print(y)
